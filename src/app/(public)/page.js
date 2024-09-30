beb73fb2efc99f0f3150736d6ff6ffc70bc4cb33
JabalJalaRaga <elizabetfiona@gmail.com>
2024-09-30 21:18:03 +0700

    ketiga

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -9,12 +9,6 @@ export default function Home() {
           <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-12 text-white" >Get Started</button>
         </div>
       </div>
-      
-      <h2 className="mt-20 rounded-lg text-center text-3xl w-full">Tentang Saya</h2>
-            
-            <p className="text-center margin-0 mx-auto w-2/3	">
-                Saya adalah mahasiswa semester 5 bernama jabal jala raga dengan jurusan manajemen informatika tinkat 3 yang kepribadian baik
-            </p>
 
     </>
     
